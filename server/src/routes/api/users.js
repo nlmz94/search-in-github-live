14b6b99410444d99862c5b1e0ffefa5b25e57310
nlmz94 <nmz94140@gmail.com>
2021-12-08 11:47:35 +0100

    best prisma

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -1,32 +1,45 @@
 import axios from "axios";
 import express, {Router} from "express";
 import {PrismaClient} from '@prisma/client';
-import {use} from "express/lib/router";
 
 const api = Router();
 api.use(express.json())
 
 api.get("/:username", async (req, response) => {
 	const {username} = req.params;
-	// Step 1 - Does User exist in our Database
-	//   If True  -> Retrieve from our Database
-	//   If False -> Request Github API https://api.github.com/users/$USERNAME
-	//            -> Store User information in our Database
 	const prisma = new PrismaClient();
-	const user = await prisma.user.findUnique({where: {login: username}})
-	if (user) {
-		response.json({data: {user}});
-	} else {
-		let gitHubUser = {errorMessage:"Error, user not found"};
+	let user = await prisma.user.findUnique({where: {login: username}})
+	if (!user) {
+		user = {errorMessage: "Error, user not found"};
 		await axios.get('https://api.github.com/users/' + username)
 			.then(res => {
 				console.log(res.data);
-				gitHubUser = res.data;
+				user = res.data;
 			})
-		await prisma.user.create({data: {login: gitHubUser.login}});
-		response.json({data: {gitHubUser}});
+		await prisma.user.create({
+			data: {
+				id: user.id,
+				login: user.login,
+				node_id: user.node_id,
+				html_url: user.html_url,
+				name: user.name,
+				company: user.company,
+				blog: user.blog,
+				location: user.location,
+				email: user.email,
+				hireable: user.hireable ?? undefined,
+				bio: user.bio,
+				twitter_username: user.twitter_username,
+				public_repos: user.public_repos,
+				public_gists: user.public_gists,
+				followers: user.followers,
+				following: user.following,
+				created_at: user.created_at,
+				updated_at: user.updated_at
+			}
+		});
 	}
-	//const result = await prisma.user.create({data: req.body})
+	response.json({data: {user}});
 });
 
 export default api;
