26aa987516ff725bff14c538a8e572deaef56e2f
nlmz94 <nmz94140@gmail.com>
2021-12-08 16:45:00 +0100

    better

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -11,32 +11,30 @@ api.get("/:username", async (req, response) => {
 	let user = await prisma.user.findUnique({where: {login: username}})
 	if (!user) {
 		user = {errorMessage: "Error, user not found"};
-		await axios.get('https://api.github.com/users/' + username)
-			.then(res => {
-				console.log(res.data);
-				user = res.data;
-			})
-		await prisma.user.create({
-			data: {
-				id: user.id,
-				login: user.login,
-				node_id: user.node_id,
-				html_url: user.html_url,
-				name: user.name,
-				company: user.company,
-				blog: user.blog,
-				location: user.location,
-				email: user.email,
-				hireable: user.hireable ?? undefined,
-				bio: user.bio,
-				twitter_username: user.twitter_username,
-				public_repos: user.public_repos,
-				public_gists: user.public_gists,
-				followers: user.followers,
-				following: user.following,
-				created_at: user.created_at,
-				updated_at: user.updated_at
-			}
+		axios.get('https://api.github.com/users/' + username).then(res => {
+			user = res.data;
+			prisma.user.create({
+				data: {
+					id: user.id,
+					login: user.login,
+					node_id: user.node_id,
+					html_url: user.html_url,
+					name: user.name,
+					company: user.company,
+					blog: user.blog,
+					location: user.location,
+					email: user.email,
+					hireable: user.hireable ?? undefined,
+					bio: user.bio,
+					twitter_username: user.twitter_username,
+					public_repos: user.public_repos,
+					public_gists: user.public_gists,
+					followers: user.followers,
+					following: user.following,
+					created_at: user.created_at,
+					updated_at: user.updated_at
+				}
+			});
 		});
 	}
 	response.json({data: {user}});
