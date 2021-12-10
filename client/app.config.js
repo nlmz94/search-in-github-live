bb5000c9d1e6b5a1882420692874a4bbc835b399
Mezdari <nmz94140@gmail.com>
2021-12-10 09:54:32 +0100

    .env example

--- client/app.config.js
+++ client/app.config.js
@@ -2,7 +2,8 @@ import 'dotenv/config';
 
 export default ({ config }) => {
   return { ...config,
-     extra: {
-      API_URL : process.env.API_URL
-  }};
+        extra: {
+            API_URL : process.env.API_URL
+        }
+    };
 };
