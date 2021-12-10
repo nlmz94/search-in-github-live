c79e0c9cde4963f8d7ebd5bc8d5095dc56ac9f68
Mezdari <nmz94140@gmail.com>
2021-12-10 09:16:09 +0100

    better api again

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -7,7 +7,7 @@ api.use(express.json())
 
 api.get("/:username", async (req, response) => {
 	const {username} = req.params;
-	const prisma = new PrismaClient();
+	const prisma = new PrismaClient({log: ['query', 'info', 'warn', 'error']})
 	let user = await prisma.user.findUnique({where: {login: username}})
 	if (!user) {
 		user = {errorMessage: "Error, user not found"};
@@ -15,7 +15,7 @@ api.get("/:username", async (req, response) => {
 			headers: {Authorization:`token ${process.env.GITHUB_TOKEN}`}
 		});
 		user = res.data;
-		prisma.user.create({data: {...res.data}});
+		await prisma.user.create({data: res.data});
 	}
 	response.json({data: {user}});
 });
